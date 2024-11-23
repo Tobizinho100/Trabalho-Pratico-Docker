89671c812906355ee0c074721306fa4295292dcf
JoaoCunha <jpedro.seara@gmail.com>
2024-11-23 14:53:39 +0000

    Initial commit

---
+++ api_rest/views.py
@@ -0,0 +1,129 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import User
+from .serializers import UserSerializer
+
+import json
+
+
+@api_view(['GET'])
+def get_users(request):
+    
+    if request.method == 'GET':
+        users = User.objects.all()
+        
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+    
+    return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT'])
+def get_by_nick(request, nick):
+
+    try:
+        user = User.objects.get(pk=nick)
+    except:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+    if request.method == 'PUT':
+
+        serializer = UserSerializer(user, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+    
+# CRUD
+@api_view(['GET','POST','PUT','DELETE'])
+def user_manager(request):
+
+#GET
+
+    if request.method == 'GET':
+
+        try:
+            if request.GET['user']:                         
+
+                user_nickname = request.GET['user']   
+
+                try:
+                    user = User.objects.get(pk=user_nickname)   
+                except:
+                    return Response(status=status.HTTP_404_NOT_FOUND) #nao encontra user
+
+                serializer = UserSerializer(user)           
+                return Response(serializer.data)            
+
+            else:
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+            
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    
+
+#CRIAR DADOS
+
+    if request.method == 'POST': #VERIFCAR SE E POST
+
+        new_user = request.data
+        
+        serializer = UserSerializer(data=new_user)
+
+        if serializer.is_valid(): #VERIFICACAO
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+        return Response(status=status.HTTP_400_BAD_REQUEST) #SE NAO FOREM VALIDOS
+
+
+
+# EDITAR DADOS (PUT)
+
+    if request.method == 'PUT': #VERIFCAR SE E PUT
+
+        nickname = request.data['user_nickname']
+
+        try:
+            updated_user = User.objects.get(pk=nickname)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = UserSerializer(updated_user, data=request.data) #COLOCA NOVOS DADOS 
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+# DELETAR DADOS (DELETE)
+
+    if request.method == 'DELETE':
+
+        try:
+            user_to_delete = User.objects.get(pk=request.data['user_nickname'])
+            user_to_delete.delete()
+            return Response(status=status.HTTP_202_ACCEPTED)
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
